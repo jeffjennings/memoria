f6d64c29e472269c076fec5d578ea5be47a2319b
Jeff Jennings <jjennings1519@gmail.com>
2024-01-17 01:52:21 -0500

    add dictionaries file

---
+++ memoria/dictionaries.py
@@ -0,0 +1,24 @@
+# new italian espresso, chapter 1, vocab
+nie_c1_v = {
+    'to study':'studiare',
+    'to want':'volere',
+    'to work':'lavorare',
+    'to inhabit':'abitare',
+    'to speak':'parlare',
+    'to visit':'visitare',
+    'to listen':'ascoltare',
+    'to be':'essere',
+    'to have':'avere',
+    'to go':'andare',
+    'to think':'pensare',
+}
+
+# grammar
+nie_c1_g = {
+
+}
+    
+# phrases
+nie_c1_p = {
+
+}
