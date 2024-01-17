0398b186ea736ac7117376d7e1769eaec2fbf1b2
Jeff Jennings <jjennings1519@gmail.com>
2024-01-17 01:52:50 -0500

    create helper functions file

---
+++ memoria/helper_funcs.py
@@ -0,0 +1,37 @@
+import webbrowser
+
+def translate(word, source_dict):
+    """Get the translation of 'word' from the dictionary 'source_dict'"""
+    translation = source_dict[word]
+
+    return translation
+
+def look_up_word(word, reference_type):
+    if reference_type in ['define', 'conjugate']:
+        if reference_type == 'define':
+            link = f"https://www.wordreference.com/iten/{word}"
+        else: 
+            link = f"https://sapere.virgilio.it/parole/coniuga-verbi/{word}"
+
+        webbrowser.open(link, new=2)
+
+    else:
+        pass
+
+def update_card(card, word, source_dict):
+    """For a button flashcard, update the card the word's translation on first click, or open a hyperlink on second click"""
+    translation = translate(word, source_dict)
+
+    if card['underline'] == 0:
+        if word.startswith('to ') or translation.startswith('to '):
+            reference_type = 'conjugate'
+        else:
+            reference_type = 'define'
+
+        look_up_word(word, reference_type)
+
+    else:
+        new_text = f"{card['text']}\n\n{translation}"
+        # update button color once it's clicked
+        card.config(text=new_text, bg='#900C3F', fg='#EEEEEE', underline=0)
+        
