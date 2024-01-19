c44555543bd41bc4dce6bb667dd8a32114351c4c
Jeff Jennings <jjennings1519@gmail.com>
2024-01-19 08:09:47 -0800

    look_up_entry: handle phrase entry type

--- memoria/helper_funcs.py
+++ memoria/helper_funcs.py
@@ -7,14 +7,29 @@ def translate(entry, source_dict):
     return source_dict[entry]
 
 
-def look_up_word(word, reference_type):
-    if reference_type in ['define', 'conjugate']:
-        if reference_type == 'define':
-            link = f"https://www.wordreference.com/iten/{word}"
-        else: 
-            link = f"https://sapere.virgilio.it/parole/coniuga-verbi/{word}"
+def look_up_entry(entry, source_dict, reference_type, entry_language='italian'):
+    """Depending on 'reference_type', look up online the definition of an Italian 
+    word or phrase 'entry', its conjugation, or show the Italian form (that we 
+    provide) on google translate to hear it spoken"""
 
-        webbrowser.open(link, new=2)
+    if reference_type == 'define':
+        link = f"https://www.wordreference.com/iten/{entry}"
+    elif reference_type == 'conjugate': 
+        link = f"https://sapere.virgilio.it/parole/coniuga-verbi/{entry}"
+    else:
+        link = "https://translate.google.com/?sl=it&tl=en&text="
+
+        # for web address, must replace whitespace in 'entry'
+        entry_no_spaces = entry.replace(" ", "%20")
+
+        if entry_language == 'english':
+            # don't want to call google translate on an english word/phrase, 
+            # as its translation may differ from our stored one
+            entry_no_spaces = translate(entry_no_spaces, source_dict)
+            
+        link += f"{entry_no_spaces}%0A&op=translate"
+
+    webbrowser.open(link, new=2)
 
     else:
         pass
