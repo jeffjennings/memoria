26c31438702ce264acdf3afc4d0e1fada83d570c
Jeff Jennings <jjennings1519@gmail.com>
2024-01-21 10:10:56 -0800

    dictionaries: example format

--- memoria/dictionaries/en_it/nie_a0_a1pt5.py
+++ memoria/dictionaries/en_it/nie_a0_a1pt5.py
@@ -1,18 +1,23 @@
 """New Italian Espresso, book 1 (CEFR levels A0 - first half of A1)"""
 
 vocab = {
-    'to study':'studiare',
-    'to want':'volere',
-    'to work':'lavorare',
-    'to inhabit':'abitare',
-    'to speak':'parlare',
-    'to visit':'visitare',
-    'to listen':'ascoltare',
-    'to be':'essere',
-    'to have':'avere',
-    'to go':'andare',
-    'to think':'pensare',
-    }
+    'ch1':{
+        'to study':'studiare',
+        'to want':'volere',
+        'to work':'lavorare',
+        'to inhabit':'abitare',
+        'to speak':'parlare',
+        'to visit':'visitare',
+        'to listen':'ascoltare',
+        'to be':'essere',
+        'to have':'avere',
+        'to go':'andare',
+        'to think':'pensare',
+        },
+    'ch2':{
+        'test it':'test en'
+    },
+}
 
 grammar = {
     "Compound prepositions":"<Table>",
