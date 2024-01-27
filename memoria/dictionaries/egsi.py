5bc925b31d72cada9e0707e310d4660fdc227ee0
Jeff Jennings <jjennings1519@gmail.com>
2024-01-26 23:07:59 -0500

    EGSI ch.1

---
+++ memoria/dictionaries/egsi.py
@@ -0,0 +1,40 @@
+"""English Grammar for Students of Italian"""
+
+vocab = {
+    'ch1':{
+        'to visit':'visitare',
+        'problem':'problema',
+        'important':'importante',
+        'moon':'luna',
+        'worried':'preoccupato',
+        'elevator':'ascensore (f.)',
+        'wolf':'lupo',
+        'mouth':'bocca',
+        'glass':'bicchiere (m.)',
+        'still/yet/more/again':'ancora',
+        'washing machine':'lavatrice',
+        'dishwasher':'lavastoviglie',
+        'to pay':'pagare',
+        'to try':'provare',
+        'to ask':'chiedere',
+        'to come':'venire',
+        'possible':'possibile',
+        'receipt':'scontrino',
+        },
+    'ch2':{  
+    },
+}
+
+grammar = {
+    'ch1':{
+    },
+    'ch2':{  
+    },    
+}
+
+phrases = {
+    'ch1':{
+    },
+    'ch2':{  
+    },    
+}
