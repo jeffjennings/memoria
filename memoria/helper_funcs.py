3fa0aca39ad3b0c038a8cea3dd2b4baccc724af4
Jeff Jennings <jjennings@flatironinstitute.org>
2024-10-07 21:57:09 -0400

    hepler_funcs: formatting

--- memoria/helper_funcs.py
+++ memoria/helper_funcs.py
@@ -63,29 +63,29 @@ def get_dictionary(book, chapter, card_type):
     return all_entries
 
 
-def translate(entry, source_dict, entry_language='English'):
-    """Get the translation of 'entry' (either English to foreign language 
+def translate(entry, source_dict, entry_language="English"):
+    """Get the translation of 'entry' (either English to foreign language
     or vice versa) from the dictionary 'source_dict'"""
 
-    if entry_language == 'English':
+    if entry_language == "English":
         translation = source_dict[entry]
     else:
         translation = [i for i in source_dict if source_dict[i] == entry][0]
-        
+
     return translation
 
 
-def look_up_entry(entry, source_dict, reference_type, entry_language='Italian'):
-    """Depending on 'reference_type', look up online the definition of a foreign 
-    language word or phrase 'entry', its conjugation, or show the foreign language 
-    form (as we provide in the `dictionaries` folder) on google translate to hear 
+def look_up_entry(entry, source_dict, reference_type, entry_language="Italian"):
+    """Depending on 'reference_type', look up online the definition of a foreign
+    language word or phrase 'entry', its conjugation, or show the foreign language
+    form (as we provide in the `dictionaries` folder) on google translate to hear
     it spoken."""
 
-    if reference_type == 'define':
-        if entry_language == 'Italian':
+    if reference_type == "define":
+        if entry_language == "Italian":
             link = f"https://www.wordreference.com/iten/{entry}"
-    elif reference_type == 'conjugate': 
-        if entry_language == 'Italian':
+    elif reference_type == "conjugate":
+        if entry_language == "Italian":
             link = f"https://sapere.virgilio.it/parole/coniuga-verbi/{entry}"
     else:
         link = "https://translate.google.com/?sl=it&tl=en&text="
@@ -93,42 +93,46 @@ def look_up_entry(entry, source_dict, reference_type, entry_language='Italian'):
         # for web address, must replace whitespace in 'entry'
         entry_no_spaces = entry.replace(" ", "%20")
 
-        if entry_language == 'English':
-            # don't want to call google translate on an English word/phrase, 
+        if entry_language == "English":
+            # don't want to call google translate on an English word/phrase,
             # as its translation may differ from our stored one
             entry_no_spaces = translate(entry, source_dict, entry_language)
-            
+
         link += f"{entry_no_spaces}%0A&op=translate"
 
     webbrowser.open(link, new=2)
 
 
-def update_card(card, entry, source_dict, entry_language='English'):
-    """For a button flashcard, update the card with the translation of 
+def update_card(card, entry, source_dict, entry_language="English"):
+    """For a button flashcard, update the card with the translation of
     'entry' on first click; open a hyperlink on second click."""
 
     translation = translate(entry, source_dict, entry_language)
 
-    if entry_language == 'English':
+    if entry_language == "English":
         english_entry = entry
         foreign_entry = translation
     else:
         foreign_entry = entry
         english_entry = translation
 
-    if card['underline'] == 0:
-        if english_entry.startswith(('to ', 'To ')):
-            reference_type = 'conjugate'
+    if card["underline"] == 0:
+        if english_entry.startswith(("to ", "To ")):
+            reference_type = "conjugate"
         elif " " in entry:
-            reference_type = 'hear'
+            reference_type = "hear"
         else:
-            reference_type = 'define'
+            reference_type = "define"
 
-        if 'Italian' in languages:
-            look_up_entry(foreign_entry, source_dict, reference_type, entry_language='Italian')
+        if "Italian" in languages:
+            look_up_entry(
+                foreign_entry,
+                source_dict,
+                reference_type,
+                entry_language="Italian",
+            )
 
     else:
         new_text = f"{card['text']}\n\n{translation}"
         # update button color once it's clicked
         card.config(text=new_text, fg=colors[6], underline=0)
-        
