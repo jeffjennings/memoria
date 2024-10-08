38d15dee32265e42b67bea29aba2088ca12bc498
Jeff Jennings <jjennings@flatironinstitute.org>
2024-10-07 21:54:55 -0400

    inputs: add person and tense

--- memoria/inputs.py
+++ memoria/inputs.py
@@ -1,19 +1,36 @@
 # languages for flashcards
 languages = ["English", "Italian"]
+person = {"I": "io", "you": "tu", "he/she": "lui/lei", "we": "noi", "y'all": "voi", "they": "loro"}
+tense = {"present indicative": "presente indicativo", "past perfect": "passato prossimo"}
 
 # content to put on flashcards
-card_types = ["Vocabulary", "Grammar", "Phrases"]
+card_types = ["Vocab: nouns, adjectives, verbs", 
+              "Vocab: adverbs, conjunctions, prepositions, pronouns",
+              "Grammar", 
+              "Phrases", 
+              "Randomly generated phrases",
+              ]
 
 # language books containing entries for each of the 'card_types',
 # and the folder name where their dictionaries are located
 books = {
-    "New Italian Espresso: A0 - A1.5":"nie_a0_a1pt5",
-        "New Italian Espresso: A1.5 - B1":"nie_a1pt5_b1",
-        "Nuovo Espresso: B2":"nie_b2",
-        "Nuovo Espresso: C1":"nie_c1",
-        "Nuovo Espresso: C2":"nie_c2",
-        "Italian Grammar in Practice":"igip",
-        "English Grammar for Students of Italian":"egsi",
-        "Pocket Italian Grammar":"pig",
-        }
+    "New Italian Espresso: A0 - A1.5": "nie_a0_a1pt5",
+    "New Italian Espresso: A1.5 - B1": "nie_a1pt5_b1",
+    "Nuovo Espresso: B2": "nie_b2",
+    "Nuovo Espresso: C1": "nie_c1",
+    "Nuovo Espresso: C2": "nie_c2",
+    "Italian Grammar in Practice": "igip",
+    "English Grammar for Students of Italian": "egsi",
+    "Pocket Italian Grammar": "pig",
+}
 
+colors = [
+    "#900C3F",
+    "#C70039",
+    "#FF5733",
+    "#17BF14",
+    "#FFC300",
+    "#DAF7A6",
+    "#2EB0ED",
+    "#1a97ff",
+]
